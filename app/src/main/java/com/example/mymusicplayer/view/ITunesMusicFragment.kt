1da4d6ec58b4077c5bd3ae15cdcde0ea9a79c39b
ElenaVeshtard <h7692306@yandex.ru>
2022-06-13 00:12:04 +0300

    Add content provider and db.

--- app/src/main/java/com/example/mymusicplayer/view/ITunesMusicFragment.kt
+++ app/src/main/java/com/example/mymusicplayer/view/ITunesMusicFragment.kt
@@ -1,12 +1,9 @@
 package com.example.mymusicplayer.view
 
-import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
